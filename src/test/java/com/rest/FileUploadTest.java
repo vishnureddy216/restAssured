46b5a57e79797684eb675ba94c1ccccd7de60f5b
vishnureddy216 <vishnureddyy1998@gmail.com>
2025-09-08 21:06:19 +0530

    fixedtestcases

--- src/test/java/com/rest/FileUploadTest.java
+++ src/test/java/com/rest/FileUploadTest.java
@@ -5,40 +5,68 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.internal.RestAssuredResponseOptionsGroovyImpl;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
+
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import utils.ApiUtils;
 import utils.Jsonutils;
-
 import java.io.IOException;
-
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
-public class FileUploadTest {
-    String DSRName = "User_" + System.currentTimeMillis();
-    RequestSpecification requestSpec;
-    ResponseSpecification responseSpec;
 
-    @BeforeClass
-    public void setUp() {
-        requestSpec = ApiUtils.getRequestSpec();
-        responseSpec = ApiUtils.getResponseSpec();
+    public class FileUploadTest {
+        String DSRName = "User_" + System.currentTimeMillis();
+        RequestSpecification requestSpec;
+        ResponseSpecification responseSpec;
+        SoftAssert softAssert = new SoftAssert();
+        public static String generatedRoomId; // class-level variable
+
+        @BeforeClass
+        public void setUp() {
+            requestSpec = ApiUtils.getRequestSpec();
+            responseSpec = ApiUtils.getResponseSpec();
+        }
+
+        @Test
+        public void testDSRCreation() throws IOException {
+            String payload = Jsonutils.getJsonPayload("src/main/resources/CreateDSRpayload.json");
+
+            Response response = given()
+                    .spec(requestSpec)
+                    .body(payload)
+                    .when()
+                    .post("v1/api/opportunity/rooms")
+                    .then()
+                    .spec(responseSpec)
+                    .log().all()
+                    .extract()
+                    .response();
+
+            // Assign to class-level variable
+            generatedRoomId = response.path("result.id");
+            System.out.println("Generated Room ID: " + generatedRoomId);
+
+            softAssert.assertAll();
+        }
+
+        @Test(priority = 2, dependsOnMethods = {"testDSRCreation"})
+        public void delete_Method() throws InterruptedException {
+            System.out.println("Deleting Room ID: " + generatedRoomId);
+            Thread.sleep(2000); // optional wait
+
+            given().spec(requestSpec)
+                    .when()
+                    .delete("v1/api/opportunity/rooms/" + generatedRoomId)
+                    .then()
+                    .statusCode(202)
+                    .body("result.status", equalTo(null));
+        }
     }
 
-    @Test(groups = { "sanity" })
-    public void testDSRCreation() throws IOException {
-        //String DSRName = "User_" + System.currentTimeMillis();
-        String payload = Jsonutils.getJsonPayload("src/main/resources/CreateDSRpayload.json");
-        //String postBody= "{\"name\":\""+DSRName+"\",\"entity_id\":\"684fd8e49a55a50394c163db\",\"company_domain\":null,\"entity_logo\":null,\"cloneable\":null,\"room_id\":null,\"entity_type\":\"OPPORTUNITY\",\"content_type\":null,\"show_team_section\":true,\"enable_mutual_action_plan\":null,\"enable_content\":true,\"enable_meeting\":false,\"enable_messaging\":true,\"share_type\":\"\",\"share_tool\":null,\"template_id\":null,\"enable_collaboration\":null,\"updateAssociation\":false}}";
-        given().spec(requestSpec)
-                .body(payload)
-                .when()
-                .post("v1/api/opportunity/rooms")
-                .then().spec(responseSpec).body("result.status",equalTo("DRAFT"));
 
-    }
-
-}
